7b88424317cd75fc55f49a102d9e85382d6516ba
Patrick Lamoureux <patrick@blackcedar.com>
2019-02-26 12:32:37 -0700

    Using Java to create 7 divs
    We worked on ordering all of the episodes with the compare function (a,b) => (a.epd > b.epis) ? 1: -1

--- persPort.js
+++ persPort.js
@@ -1,23 +1,27 @@
 import{ films } from './films.js'
-// import{people} from './people.js'
-// import {planets} from './planets.js'
-// import {species} from './species.js'
-// import {starships} from './species.js'
-// import {vehicles} from './vehicles'
-const result = films.filter(film => film.episode_id)
+
+// const result = films.filter(film => film.episode_id)
+// console.log(result)
+films.sort((a,b)=> (a.episode_id> b.episode_id) ? 1 : -1)
+const intro = document.querySelector('.intro')
+
+const result = films.sort(film => film.episode_id)
+
 console.log(result)
-// const intro = document.querySelector('.intro')
-// films.sort(function(episode_id){
-//   return episode_id
-// }
+
 films.forEach((film) => {
+
+ let tile = document.createElement('div')
+
  let titleElement = document.createElement('h1')
  let crawlElement = document.createElement('h3')
+
  titleElement.textContent = film.title
  crawlElement.textContent = film.opening_crawl
- intro.appendChild(titleElement)
- intro.appendChild(crawlElement)
- 
+
+ tile.appendChild(titleElement)
+ tile.appendChild(crawlElement)
+ intro.appendChild(tile)
 })
 // let titleElement = document.querySelector(".title")
 // let crawlElement = document.querySelector(".crawl")
