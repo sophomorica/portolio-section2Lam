8956f8ab4e85c3b4e3527576d045e5905bcd0456
Patrick Lamoureux <patrick@blackcedar.com>
2019-03-05 12:02:34 -0700

    got men and women to show up

--- Scripts/swpeople.js
+++ Scripts/swpeople.js
@@ -8,11 +8,34 @@ console.log(men, women, other)
 
 const mainContainer = document.createElement('div')
 mainContainer.className = 'container'
+
 men.forEach((man) => {
 
   let manElement = document.createElement('div')
   manElement.className = 'box'
   manElement.textContent = man.name
+  let eyeColor = document.createElement('p')
+  eyeColor.textContent = man.eye_color
+  manElement.appendChild(eyeColor)
+  mainContainer.appendChild(manElement)
+  
+})
+
+// women.forEach((woman)=> {
+//   let womanElement = document.createElement('div')
+  
+// womanElement.className = 'box'
+// womanElement.textContent = woman.name
+// mainContainer.appendChild(womanElemnet)
+// })
+women.forEach((man) => {
+
+  let manElement = document.createElement('div')
+  manElement.className = 'box'
+  manElement.textContent = man.name
+  let eyeColor = document.createElement('p')
+  eyeColor.textContent = man.eye_color
+  manElement.appendChild(eyeColor)
   mainContainer.appendChild(manElement)
   
 })
