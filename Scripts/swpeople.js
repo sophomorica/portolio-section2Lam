d92d3cb3ccbca2b008eef2d4fc1c283dfa26bf13
Patrick Lamoureux <patrick@blackcedar.com>
2019-03-14 12:32:58 -0600

    organizing a little bit

--- Scripts/swpeople.js
+++ Scripts/swpeople.js
@@ -4,7 +4,8 @@ import { planets } from '../data/planets.js'
 
 
 //character act string funciton MDN
-//Slice string method MDN "how to get a section of a string from an index"
+//Slice string method MDN "how to get a section of a string from an index" . We did thus 
+
 const getLastNumber = (url) => {
   let end = url.lastIndexOf('/')
   console.log(end)
@@ -14,7 +15,10 @@ const getLastNumber = (url) => {
  }
 return (url.slice(start, end))
 }
-//we are going to import from one array then within that loop we will do search into another array
+
+//------------------------we are going to import from one array then within that loop we will do search into another array in order to find the homeworlds and then create a new array to be called on later ------------------------------
+
+
 const allHomeWorlds = people.map(person => {
   let foundWorld = planets.find(planet =>{
     return planet.url === person.homeworld
@@ -23,15 +27,11 @@ const allHomeWorlds = people.map(person => {
   return {
     name: person.name, 
     home: foundWorld.name,
-    
+    gender: person.gender,
     imagePath: `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`,
-  force: person.force,
+    force: person.force,
 }
 })
-
-
-
-console.log(allHomeWorlds)
 
 
 
@@ -45,10 +45,9 @@ console.log(allHomeWorlds)
 //setting the variables of men women and other in order to later use them in functions
 //each variable is assigned a value by reaching into the imported "people" js
 
-const men = people.filter(person=> person.gender ==='male')
-const women = people.filter(person => person.gender ==='female')
-const other = people.filter(person => (person.gender ==='n/a') || (person.gender ==='hermaphrodite') || (person.gender ==='other'))
-// console.log(men, other,women)
+const men = allHomeWorlds.filter(person=> person.gender ==='male')
+const women = allHomeWorlds.filter(person => person.gender ==='female')
+const other = allHomeWorlds.filter(person => (person.gender ==='n/a') || (person.gender ==='hermaphrodite') || (person.gender ==='other'))
 
 const lightSide = people.filter(person=> person.force ==='light')
 const darkSide = people.filter(person=> person.force === 'dark')
@@ -66,45 +65,44 @@ const mainContainer = document.createElement('div')
 mainContainer.className = 'container'
 
 
-//trying to get a button to filter the genders
+//------pulling the HTML id to assign variables to use in the event listener------
+
 var females = document.getElementById('females')
 var males = document.getElementById('males')
 
 
-// mouse event for the button to filter out just the males--------------------
+// --------mouse event for the button to filter out just the males--------------------
 
-// males.addEventListener('click', filterMales)
+males.addEventListener('click', filterMales)
 
-// function filterMales(){
+function filterMales(){
 
-//   men.forEach((man) => {
-//   let manElement = document.createElement('div')
-//   let imageElement = document.createElement('img')
+  men.forEach((man) => {
+  let manElement = document.createElement('div')
+  let imageElement = document.createElement('img')
 
-// manElement.className = 'personBox'
-// manElement.textContent = man.name
+manElement.className = 'personBox'
+manElement.textContent = man.name
 
-// imageElement.src= man.imagePath
+imageElement.src= imagePath
 
-// manElement.appendChild(imageElement)
-// mainContainer.appendChild(manElement)
-// })
-// males.removeEventListener('click', filterMales)
+manElement.appendChild(imageElement)
+mainContainer.appendChild(manElement)
+})
+males.removeEventListener('click', filterMales)
 
-// }
-//trying to add the darkside light side function--------------
+}
 
 
-//event listener to filter out the females---------------------------------------
+
+//----------event listener to filter out the females----------------------------------
+
 // females.addEventListener('click', filterFemales)
-
 // function filterFemales(){ 
- 
 // women.forEach((woman) => { 
 //   let womanElement = document.createElement('div')
 //   let imageElement = document.createElement('img')
 //   // let planetElement = document.createElement('p')
-
   
 // womanElement.className = 'box'
 // womanElement.textContent = woman.name
@@ -124,48 +122,37 @@ var males = document.getElementById('males')
 
 //end of my trials-----------------------------------------------------------------
 
-allHomeWorlds.forEach((person) => {
+// allHomeWorlds.forEach((person) => {
   
-  let personElement = document.createElement('div')
-  let planetElement = document.createElement ('p')
-  let imageElement = document.createElement ('img')
+//   let personElement = document.createElement('div')
+//   let planetElement = document.createElement ('p')
+//   let imageElement = document.createElement ('img')
   
-
-  if (person.force ==='light'){
-    personElement.className = 'box lightSide personBox'
-  } 
-  if (person.force ==='dark'){
-    personElement.className = 'box darkSide personBox'
-  }
-  if (person.force ==='yellow'){
-    personElement.className = 'box yellow personBox'
-  }
+// // assigning different classes to elements with certain element ID's
+//   if (person.force ==='light'){
+//     personElement.className = 'box lightSide personBox'
+//   } 
+//   if (person.force ==='dark'){
+//     personElement.className = 'box darkSide personBox'
+//   }
+//   if (person.force ==='yellow'){
+//     personElement.className = 'box yellow personBox'
+//   }
  
 
-  // personElement.className = 'personBox'
-  personElement.textContent = person.name
-  planetElement.textContent = person.home
+ 
+//   personElement.textContent = person.name
+//   planetElement.textContent = person.home
   
  
 
-  imageElement.src = person.imagePath
+//   imageElement.src = person.imagePath
 
   
-  personElement.appendChild(imageElement)
-  personElement.appendChild(planetElement)
-  mainContainer.appendChild(personElement)
+//   personElement.appendChild(imageElement)
+//   personElement.appendChild(planetElement)
+//   mainContainer.appendChild(personElement)
   
-}) 
-
-
-// women.forEach((woman)=> {
-//   let womanElement = document.createElement('div')
-  
-// womanElement.className = 'box'
-// womanElement.textContent = woman.name
-// mainContainer.appendChild(womanElement)
-// })
-
-
+// }) 
 
 document.body.appendChild(mainContainer)
