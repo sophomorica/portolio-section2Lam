875e7785c3d817f7c120c34b891593f0df6138ea
Patrick Lamoureux <patrick@blackcedar.com>
2019-03-26 12:46:16 -0600

    organized forlders and added "type module for js

--- Scripts/pokeCards.js
+++ Scripts/pokeCards.js
@@ -1,4 +1,4 @@
-import { pokemon } from "./data/pokemon.js/"
+import { pokemon } from "../data/pokemon.js"
 
 
 console.log(pokemon)
@@ -13,19 +13,21 @@ const mainContainer = document.querySelector('.container')
     let title = document.createElement('h2')
     let image = document.createElement('img')
     
-
-    title.textContent(title) = pokeData.name
+    let upperName = pokeData.name.charAt(0).toUpperCase()+ pokeData.name.slice(1)
+    title.textContent = upperName
     image.src = pokeData.sprites.front_shiny
-    card.appendChild(card)
+    card.appendChild(title)
+    card.appendChild(image)
+    mainContainer.appendChild(card)
   }
 
 pokemon.forEach(singleMon => {
-  fetch(singleMon.name)
+  fetch(singleMon.url)
   .then(function(response){
     return response.json()
   })
  .then(function(myJson){
-   console.log(myJson)
+ createPokeCard(myJson)
  })
 })
 
