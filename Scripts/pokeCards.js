f329d5bfe7ab5dc8fd3f1687950e91ff344b17e2
Patrick Lamoureux <patrick@blackcedar.com>
2019-03-26 12:56:09 -0600

    organized pictures and fixing pokemon js

--- Scripts/pokeCards.js
+++ Scripts/pokeCards.js
@@ -7,19 +7,19 @@ console.log(pokemon)
 
 const mainContainer = document.querySelector('.container')
 
-  function createPokeCard(pokeData) {
-    let card = document.createElement('div')
-    card.className = 'box'
-    let title = document.createElement('h2')
-    let image = document.createElement('img')
-    
-    let upperName = pokeData.name.charAt(0).toUpperCase()+ pokeData.name.slice(1)
-    title.textContent = upperName
-    image.src = pokeData.sprites.front_shiny
-    card.appendChild(title)
-    card.appendChild(image)
-    mainContainer.appendChild(card)
-  }
+function createPokeCard(pokeData) {
+  let card = document.createElement('div')
+  card.className = 'box'
+  let title = document.createElement('h2')
+  let image = document.createElement('img')
+  
+  let upperName = pokeData.name.charAt(0).toUpperCase()+ pokeData.name.slice(1)
+  title.textContent = upperName
+  image.src = pokeData.sprites.front_shiny
+  card.appendChild(title)
+  card.appendChild(image)
+  mainContainer.appendChild(card)
+}
 
 pokemon.forEach(singleMon => {
   fetch(singleMon.url)
@@ -27,7 +27,7 @@ pokemon.forEach(singleMon => {
     return response.json()
   })
  .then(function(myJson){
- createPokeCard(myJson)
+  createPokeCard(myJson)
  })
 })
 
