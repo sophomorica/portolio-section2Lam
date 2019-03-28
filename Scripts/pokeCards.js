20ae1b14090edf11b9d19412e1b932b67e7c601b
Patrick Lamoureux <patrick@blackcedar.com>
2019-03-28 12:03:32 -0600

    adding a repository for images and trying to resol

--- Scripts/pokeCards.js
+++ Scripts/pokeCards.js
@@ -1,5 +1,10 @@
 import { pokemon } from "../data/pokemon.js"
 
+class Pokemon {
+  constructor(id){
+      this.id= id
+  }
+}
 
 console.log(pokemon)
 //hello my name is patrick
@@ -11,25 +16,45 @@ const mainContainer = document.querySelector('.container')
 function createPokeCard(pokeData) {
   let card = document.createElement('div')
   card.className = 'box'
-  let title = document.createElement('h2')
+  let figure = document.createElement('figure')
+  let caption = document.createElement('figcaption')
+  // let title = document.createElement('h2')
   let image = document.createElement('img')
   
   let upperName = pokeData.name.charAt(0).toUpperCase()+ pokeData.name.slice(1)
-  title.textContent = upperName
+
+  caption.textContent = upperName
   image.src = pokeData.sprites.front_shiny
+  if(pokeData.id !==0){
+      image.src = `../PokeImages/images/${pokeData.id}${upperName}.png`
+  } else {
+    image.src = `../PokeImages/images/pokeball.png`
+  }
+  figure.appendChild(image )
+
   card.appendChild(title)
   card.appendChild(image)
   mainContainer.appendChild(card)
 }
 
-pokemon.forEach(singleMon => {
-  fetch(singleMon.url)
-  .then(function(response){
-    return response.json()
-  })
- .then(function(myJson){
-  createPokeCard(myJson)
- })
-})
+// pokemon.forEach(singleMon => {
+//   fetch(singleMon.url)
+//   .then(function(response){
+//     return response.json()
+//   })
+//  .then(function(myJson){
+//   createPokeCard(myJson)
+//  })
+// })
 
- 
+//this is the same as above but better
+function fetchSinglePokemon(id){
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  .then(response => response.json())
+  .then(myJson => createPokeCard(myJson))
+}
+ const newPOkemonButton = document.querySelector('button')
+ newPOkemonButton.addEventListener('click', function(){
+   let pokemonID = prompt('Enter the ID of an existing Pokemon')
+   createPokeCard( new Pokemon(pokemonID))
+ });
