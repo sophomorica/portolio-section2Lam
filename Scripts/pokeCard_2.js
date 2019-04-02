7252d51af4a45a22e286193ef9086cd007fb595f
Patrick Lamoureux <patrick@blackcedar.com>
2019-04-02 12:08:19 -0600

    trying to get the cards to show and the css

--- Scripts/pokeCard_2.js
+++ Scripts/pokeCard_2.js
@@ -9,19 +9,12 @@ class Pokemon {
 const mainContainer = document.querySelector(".container");
 
 function cardFront(pokeData){
-
-}
-
-function createPokeCard(pokeData) {
-  let card = document.createElement("div")
-  card.className = "box"
+  let cardFront = document.createElement("div")
+  cardBack.className = 'card__face--front card__face'
   let figure = document.createElement("figure")
   let caption = document.createElement("figcaption")
   let image = document.createElement("img")
-
-  // let upperName = pokeData.name.charAt(0).toUppercase() + pokeData.name.slice(1)
-
-caption.textContent = pokeData.name;
+  caption.textContent = pokeData.name;
 
   if (pokeData.id !== 0) {
     image.src = `../images/${pokeData.imageID}${pokeData.name}.png`;
@@ -31,10 +24,45 @@ caption.textContent = pokeData.name;
 
     figure.appendChild(image)
     figure.appendChild(caption)
-    card.appendChild(figure)
-    mainContainer.appendChild(card)
+    cardFront.appendChild(figure)
+    return cardFront
+}
+
+function cardBack(pokeData){
+  let cardBack = document.createElement('div')
+  cardBack.className = 'card__face--back card__face'
+  return cardBack
 
 }
+
+
+function createPokeCard(pokeData) {
+  let scene = document.createElement('div')
+  let card = document.createElement("div")
+  scene.className = 'scene'
+  card.className = 'card'
+
+  card.appendChild(cardFront(pokeData))
+  card.appendChild(cardBack(pokeData))
+  scene.appendChild(card)
+  mainContainer.appendChild(scene)
+}
+
+  // let upperName = pokeData.name.charAt(0).toUppercase() + pokeData.name.slice(1)
+
+
+
+
+
+pokemon.forEach(singleMon => {
+  fetch(singleMon.url)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(myJson){
+    createPokeCard(matchIdToImage(myJson))
+  })
+})
 
 function matchIdToImage(aPokemon){
   if (aPokemon.id < 10) {
@@ -50,18 +78,6 @@ function matchIdToImage(aPokemon){
   aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1);
   return aPokemon
 }
-
-pokemon.forEach(singleMon => {
-  fetch(singleMon.url)
-  .then(function(response){
-    return response.json()
-  })
-  .then(function(myJson){
-    createPokeCard(matchIdToImage(myJson))
-  })
-})
-
-
 
 function fetchSinglePokemon(id) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
