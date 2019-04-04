3eb1042a5474e72cbc8c2dce0131fc5b9ba55a04
Patrick Lamoureux <patrick@blackcedar.com>
2019-04-03 20:42:54 -0600

    added some notes to my code

--- Scripts/pokemonPractice.js
+++ Scripts/pokemonPractice.js
@@ -3,7 +3,8 @@ import { pokemon } from '../data/pokemon.js'
 
 
 
-  
+  //-----Below is an attempt at editing the pokemon name fetched from the API.  Some of the names have a dash('-') which disrupts the get image function.------------//
+
   // const deleteForM = (name) =>{
   //   let end = name.lastIndexOf('-')
    
@@ -14,17 +15,27 @@ import { pokemon } from '../data/pokemon.js'
   //  console.log(start + " " + end)
   // }
 
+//--------Below is the the functions that add elements to the actual document/HTML site.--------------//
+
 
   const mainContainer = document.querySelector('.container')
 
   function createPokeCard(pokeData){
     let card = document.createElement('div')
     let title = document.createElement('h2')
+    let image = document.createElement('img')
     
     title.textContent = pokeData.name + " " + pokeData.id
+    image.src = pokeData.sprites.front_shiny
+    card.appendChild(image)
     card.appendChild(title)
+   
     mainContainer.appendChild(card)
   }
+
+
+//---this sets the initial 25 pokemon found in my pokemon.js file and makes a card for each of the URLs found in the file--------------
+
 
 pokemon.forEach(singleMon=>{
   fetch(singleMon.url)
@@ -35,13 +46,37 @@ pokemon.forEach(singleMon=>{
       createPokeCard(myJson)
     })
 })
+
+//---this is for reference to what is accessable in the array of information---//
+
+
 fetch("https://pokeapi.co/api/v2/pokemon/32/")
-  .then(function(response_2){
-    return response_2.json()
+  .then(function(response){
+    return response.json()
   })
   .then(function(myJson_2){
-    console.log(myJson)
+    console.log(myJson_2)
   })
 
+//-------below is the code that first creates a fetch function to dig into all of the hidden elements from the api in the URL,--------------
 
-  
+
+function fetchSinglePokemon(id){
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(myJson){
+    createPokeCard(myJson)
+  })
+}
+
+//----this is the button function that uses the fetch from the api URL then let's you add a new card from the pokemon ID-----------------//
+
+
+const newPokeButton = document.querySelector('button')
+
+newPokeButton.addEventListener('click', function(){
+  let pokemonID = prompt("Enter the number of a Pokemon:")
+  fetchSinglePokemon(pokemonID)
+})
