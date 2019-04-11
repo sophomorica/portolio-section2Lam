f8bbc7dcf6416ed5b495b92b72c3d4a9ad748cfd
Patrick Lamoureux <patrick@blackcedar.com>
2019-04-11 12:36:30 -0600

    fixing the images

--- Scripts/pokeCard_2.js
+++ Scripts/pokeCard_2.js
@@ -19,11 +19,11 @@ import { pokemon } from "../data/pokemon.js";
 const mainContainer = document.querySelector(".container");
 
 function cardFront(pokeData){
-  let cardFront = document.createElement("div")
-  cardFront.className = 'card__face'
-  let figure = document.createElement("figure")
-  let caption = document.createElement("figcaption")
-  let image = document.createElement("img")
+  let cardFront = document.createElement('div')
+  cardFront.className = 'card__face card__face--front'
+  let figure = document.createElement('figure')
+  let caption = document.createElement('figcaption')
+  let image = document.createElement('img')
   
   caption.textContent = pokeData.name;
   if (pokeData.id !== 0) {
@@ -77,12 +77,12 @@ function createPokeCard(pokeData) {
   let card = document.createElement('div')
   card.className = 'card'
 
-  card.appendChild(cardFront(pokeData))
-  card.appendChild(cardBack(pokeData))
-
   card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
-});
+})
+card.appendChild(cardFront(pokeData))
+card.appendChild(cardBack(pokeData))
+
   scene.appendChild(card)
   mainContainer.appendChild(scene)
 }
@@ -101,27 +101,31 @@ pokemon.forEach(singleMon => {
 
 function matchIdToImage(aPokemon) {
   if (aPokemon.id === 0) {
-    aPokemon.imageID = 0
+      aPokemon.imageID = 0
   }
-  if (aPokemon.id < 10) {
-    aPokemon.imageID = '00' + aPokemon.id
+  if(aPokemon.id < 10) {
+      aPokemon.imageID = "00" + aPokemon.id
   }
-  if (aPokemon.id > 9 && aPokemon.id < 100) {
-    aPokemon.imageID = '0' + aPokemon.id
+  if(aPokemon.id > 9 && aPokemon.id < 100 ) {
+      aPokemon.imageID = "0" + aPokemon.id
   }
-  if (aPokemon.id > 99) {
-    aPokemon.imageID = aPokemon.id
+  if(aPokemon.id > 99) {
+      aPokemon.imageID = aPokemon.id
   }
+
   if(aPokemon.name === "mr-mime") {
       aPokemon.name = "mr. Mime"
   }
-  let dash = aPokemon.name.indexOf('-')
-  if(dash !== -1) {
-      aPokemon.name = aPokemon.name.slice(0,dash)
+
+  let dashIndex = aPokemon.name.indexOf('-')
+  if (dashIndex !== -1) {
+      // Found a few pokemon who have a dash in their name
+      aPokemon.name = aPokemon.name.slice(0, dashIndex)
   }
   aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1)
   return aPokemon
 }
+
 
 
 function fetchSinglePokemon(id) {
@@ -164,23 +168,27 @@ class Pokemon {
 
 const newPokemonButton = document.querySelector("button");
 
-newPokemonButton.addEventListener("click", function() {
-
-  let pokeName = prompt("Enter a name for a new pokemon:");
+newPokemonButton.addEventListener("click", function() { let pokeName = prompt("Enter a name for a new pokemon:");
   createPokeCard(new Pokemon(pokeName))
-  // fetchSinglePokemon(pokemonID);
 });
 
 
 
-const newPokemonButtonFetch = document.querySelector(".button_2")
+const fetchPokemonbyID = document.querySelector(".button_2")
 
-newPokemonButtonFetch.addEventListener('click',function(){
+fetchPokemonbyID.addEventListener('click',function(){
   let pokemonID = prompt('Enter the ID of an existing Pokemon:')
   fetchSinglePokemon(pokemonID)
 })
-// const newPokemonButtonFetch = document.querySelector('button_2')
-//   newPokemonButtonFetch.addEventListener('click', function() {
+// const fetchPokemonbyID = document.querySelector('button_2')
+//   fetchPokemonbyID.addEventListener('click', function() {
 //     let pokemonID = prompt('Enter an ID of an existing pokemon:')
 //     fetchSinglePokemon(pokemonID)
 //   });
+
+
+// const removeSongCards = () => {
+//   while (musicWrapper.firstChild{
+//     musicWrapper.removeChild(musicWrapper.firstChild);
+//   }
+// }
